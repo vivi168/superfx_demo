1287fb8bf39716d6ced7c6936a14b50e14536703
Vivien B <vivienbihl@gmail.com>
2022-09-25 20:54:46 +0200

    Test things

--- superfx/main.asm
+++ superfx/main.asm
@@ -5,4 +5,13 @@ entry:
     iwt r1,#4567
     add r1
     nop
+
+    ibt r1,#01
+    from r1
+    romb
+    iwt r14,#0000
+    to r2
+    ; getb ; movb Rd,[romb:r14]    ;hi=zero-expanded ; need ROM access
+
+
     stop
